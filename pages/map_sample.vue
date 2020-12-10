ca4a04243f3970976f9bf95a4e894dfd056e5b44
ka_ueno <ka.ueno@acca-int.co.jp>
2020-12-10 18:57:16 +0900

    .

---
+++ pages/map_sample.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        hellp
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueMindmap from 'vue-mindmap'
+// You need a specific loader for CSS files like https://github.com/webpack/css-loader
+import 'vue-mindmap/dist/vue-mindmap.css'
+
+Vue.use(VueMindmap)
+</script>
